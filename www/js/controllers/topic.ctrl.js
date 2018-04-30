7c6e542aa34ffa4619194f3653fc07dbc96edb4e
Oran <oran1284@gmail.com>
2018-04-30 09:02:03 +0300

    add back button

--- www/js/controllers/topic.ctrl.js
+++ www/js/controllers/topic.ctrl.js
@@ -7,29 +7,18 @@ function TopicController($scope, $state, DataService) {
   $scope.topic = DataService.currentBook.topic;
   console.log($scope.topic);
   $scope.menu = true;
-
-  $scope.openmenu = function () {
-    $scope.menu = false;
-    console.log($scope.menu);
+  $scope.goBack = function () {
+    console.log("poel");
+    window.history.back();
   }
 
-  $scope.closemenu = function () {
-    $scope.menu = true;
-    console.log($scope.menu);
-  }
 
   $scope.goToTopicContent = function (subject) {
-
     DataService.currentTopic.subject = subject;
     // let numOfTheTOpic = subject.number;
     // console.log(subject);
     let test = subject.topicName;
     console.log(test);
-
-
-    $scope.goBack = function () {
-      window.history.back();
-    }
 
     $state.go(test);
 
