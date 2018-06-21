3c9269231de99e546cc7859a96345e440598ba97
Oran <oran1284@gmail.com>
2018-06-21 10:23:11 +0300

    add adjust game fixing

--- www/js/controllers/adjustDate.ctrl.js
+++ www/js/controllers/adjustDate.ctrl.js
@@ -5,41 +5,44 @@ angular
 adjustDatecontroller.$inject = ["$scope", "$state", "DataService", "$timeout"];
 
 function adjustDatecontroller($scope, $state, DataService, $timeout) {
+  $scope.dateLife = [true, true, true];
+  $scope.dateLifeCounter = 3;
+  var eventsArry = DataService.adjustDateGame;
+  var iDate = randomNumber(3);
+  var randomEvent = randomNumber(eventsArry.length - 1);
+  var theUsedEvents = [];
+  theUsedEvents.push(randomEvent);
+  $scope.events = eventsArry[randomEvent].event;
+  $scope.date = eventsArry[randomEvent].dates[iDate].date;
+  var datefitEvent = eventsArry[randomEvent].dates[iDate].type;
+  $scope.ifDateNotFit = false;
+  $scope.ifDateFit = false;
+  $scope.win = false;
+  $scope.loose = false;
+
+  //Back button - going back to exercises pg
   $scope.goBackExercise = function () {
     $scope.restartadjustDate();
     $state.go("exercise");
   };
 
+  //random num from 0 - num
   function randomNumber(num) {
     var x;
     x = Math.floor(Math.random() * num + 0);
     return x;
   }
 
+  //this function checks if num exist in the arry
   function ifNumExist(array, num) {
     for (var i = 0; i < array.length; i++) {
       if (array[i] === num) return array[i] === num;
     }
+
     return false;
   }
 
-  $scope.dateLife = [true, true, true];
-  $scope.dateLifeCounter = 3;
-  console.log($scope.dateLife);
-
-  var eventsArry = DataService.adjustDateGame;
-
-  var iDate = randomNumber(3);
-  var randomEvent = randomNumber(eventsArry.length - 1);
-
-  var theUsedEvents = [];
-  theUsedEvents.push(randomEvent);
-  console.log(theUsedEvents);
-
-  $scope.events = eventsArry[randomEvent].event;
-  $scope.date = eventsArry[randomEvent].dates[iDate].date;
-  var datefitEvent = eventsArry[randomEvent].dates[iDate].type;
-
+  //black right arrow, lists to the next date
   $scope.nextDate = function () {
     //console.log("NEXT");
     if (iDate === 3) {
@@ -49,6 +52,7 @@ function adjustDatecontroller($scope, $state, DataService, $timeout) {
     datefitEvent = eventsArry[randomEvent].dates[iDate].type;
   };
 
+  //black left arrow, lists to the previous date
   $scope.previousDate = function () {
     if (iDate === 0) {
       iDate = 3;
@@ -58,53 +62,7 @@ function adjustDatecontroller($scope, $state, DataService, $timeout) {
     datefitEvent = eventsArry[randomEvent].dates[iDate].type;
   };
 
-  function addCorrectAnswer() {
-    document.getElementById("date").classList.add("correctAnswer");
-    document.getElementById("date").classList.add("adjustDate--correctAnswer");
-    document.getElementById("event").classList.add("adjustDate--correctAnswer");
-  }
-
-  function removeCorrectAnswer() {
-    document.getElementById("date").classList.remove("correctAnswer");
-    document
-      .getElementById("date")
-      .classList.remove("adjustDate--correctAnswer");
-    document
-      .getElementById("event")
-      .classList.remove("adjustDate--correctAnswer");
-  }
-
-  function addInCorrectAnswer() {
-    document.getElementById("date").classList.add("inCorrectAnswer");
-    document
-      .getElementById("date")
-      .classList.add("adjustDate--inCorrectAnswer");
-    document
-      .getElementById("event")
-      .classList.add("adjustDate--inCorrectAnswer");
-  }
-
-  function removeInCorrectAnswer() {
-    document.getElementById("date").classList.remove("inCorrectAnswer");
-    document
-      .getElementById("date")
-      .classList.remove("adjustDate--inCorrectAnswer");
-    document
-      .getElementById("event")
-      .classList.remove("adjustDate--inCorrectAnswer");
-  }
-
-  function displayFailDiv() {
-    console.log("display FAIL DIV");
-    document.getElementById("adjustDate--FailID").classList.add("showElement");
-  }
-
-  function displaySuccessDiv() {
-    document
-      .getElementById("adjustDate--SuccessID")
-      .classList.add("showElement");
-  }
-
+  //displays new event and new date after every time the player checks his answer
   $scope.displayData = function (randomEvent, iDate) {
     $scope.events = eventsArry[randomEvent].event;
     $scope.date = eventsArry[randomEvent].dates[iDate].date;
@@ -112,86 +70,87 @@ function adjustDatecontroller($scope, $state, DataService, $timeout) {
     iDate = randomNumber(3);
   };
 
+  //restart the game
   $scope.restartadjustDate = function () {
     theUsedEvents = [];
     iDate = randomNumber(3);
     randomEvent = randomNumber(eventsArry.length - 1);
+    theUsedEvents.push(randomEvent);
+    $scope.displayData(randomEvent, iDate);
     $scope.dateLife = [true, true, true];
     $scope.dateLifeCounter = $scope.dateLife.length;
-    document
-      .getElementById("adjustDate--FailID")
-      .classList.remove("showElement");
-    document
-      .getElementById("adjustDate--SuccessID")
-      .classList.remove("showElement");
+    datefitEvent = eventsArry[randomEvent].dates[iDate].type;
+    $scope.win = false;
+    $scope.loose = false;
   };
 
   $scope.checkAnswer = function () {
     iDate = randomNumber(3);
-    if (theUsedEvents.length == eventsArry.length) {
+    if (theUsedEvents.length == 6) {
       if (datefitEvent) {
-        addCorrectAnswer();
-        setTimeout(
-          ($scope.nextQ1 = function () {
-            removeCorrectAnswer();
+        $scope.ifDateFit = true;
+        $timeout(() => {
+          $scope.ifDateFit = false;
+          $scope.restartadjustDate();
+          $scope.win = true;
+          console.log($scope.win);
+        }, 1000);
+        //$state.go("adjustDateFinishPg");
+      } else if (!datefitEvent) {
+        $scope.dateLifeCounter--;
+        if ($scope.dateLifeCounter == 0) {
+          $scope.ifDateNotFit = true;
+          $timeout(() => {
+            $scope.ifDateNotFit = false;
             $scope.restartadjustDate();
-            displaySuccessDiv();
-            //$state.go("adjustDateFinishPg");
-          }),
-          1000
-        );
-      } else {
-        addInCorrectAnswer();
-        setTimeout(
-          ($scope.nextQ = function () {
-            removeInCorrectAnswer();
+            $scope.loose = true;
+          }, 1000);
+        } else {
+          $scope.ifDateFit = true;
+          $timeout(() => {
+            $scope.ifDateFit = false;
             $scope.restartadjustDate();
-            displaySuccessDiv();
-            // $state.go("adjustDateFinishPg");
-          }),
-          1000
-        );
+            $scope.win = true;
+          }, 1000);
+        }
       }
     } else {
       while (ifNumExist(theUsedEvents, randomEvent)) {
         randomEvent = randomNumber(eventsArry.length);
       }
+      theUsedEvents.push(randomEvent);
 
       if (datefitEvent) {
-        //console.log("YOU RIGHT");
-        theUsedEvents.push(randomEvent);
-        addCorrectAnswer();
-        setTimeout(
-          ($scope.nextQ1 = function () {
-            removeCorrectAnswer();
-          }),
-          1000
-        );
+        console.log("YOU RIGHT");
+        //theUsedEvents.push(randomEvent);
+        $scope.ifDateFit = true;
+        $timeout(() => {
+          $scope.ifDateFit = false;
+        }, 1000);
       } else {
-        //console.log("YOU FAIL");
-        theUsedEvents.push(randomEvent);
+        console.log("YOU FAIL");
+        //theUsedEvents.push(randomEvent);
         $scope.dateLifeCounter--;
         $scope.dateLife[$scope.dateLifeCounter] = false;
+        //console.log($scope.dateLifeCounter);
         if ($scope.dateLifeCounter == 0) {
           $scope.restartadjustDate();
-          displayFailDiv();
-
-          // $state.go("adjustDateFinishFail");
+          $scope.loose = true;
         }
-        addInCorrectAnswer();
-        setTimeout(
-          ($scope.nextQ = function () {
-            removeInCorrectAnswer();
-          }),
-          1000
-        );
+        $scope.ifDateNotFit = true;
+        $timeout(() => {
+          $scope.ifDateNotFit = false;
+        }, 1000);
       }
       $timeout(() => {
         $scope.displayData(randomEvent, iDate);
+        datefitEvent = eventsArry[randomEvent].dates[iDate].type;
       }, 1000);
     }
   };
 
+  //after finishing the game
+  //the back button (חזרה)
   $scope.backtoExercisePg = function () {
     $scope.restartadjustDate();
     $state.go("exercise");
